b23cf24bc544060931712c4048bb732b749f41ca
Miguel Rodriguez <merodrig@gmail.com>
2020-10-30 12:04:58 -0500

    Hello BOOST compute

--- HelloWorld/src/HelloWorld.cpp
+++ HelloWorld/src/HelloWorld.cpp
@@ -5,11 +5,40 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
+/*
 #include <iostream>
+#include <boost/format.hpp>
+
 using namespace std;
+using namespace boost;
+
+int main()
+{
+    unsigned int arr[5] = { 0x05, 0x04, 0xAA, 0x0F, 0x0D };
+
+    cout << format("%02X-%02X-%02X-%02X-%02X")
+            % arr[0]
+            % arr[1]
+            % arr[2]
+            % arr[3]
+            % arr[4]
+         << endl;
+}
+
+*/
+#include <iostream>
+#include <boost/compute/core.hpp>
+
+using namespace std;
+namespace compute = boost::compute;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	// get default device and setup context
+	compute::device device = compute::system::default_device();
+
+	// print the device's name
+	std::cout << "hello from " << device.name() << std::endl;
+	//cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
+
